8ef87eb64b567ab4b40c0a803390b759b36448b7
Caleb Sander <caleb.sander@gmail.com>
2016-10-29 00:12:05 -0400

    Added assembler with testing program

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for asm in $(find . -name "*.asm"); do
+ 	base=${asm/.asm/}
+ 	~/nand2tetris/tools/Assembler.bat $asm > /dev/null #make this .sh to run on linux
+ 	mv $base.hack $base-actual.hack
+ 	./assemble.js $asm
+ 	diff --ignore-all-space $base.hack $base-actual.hack
+done
